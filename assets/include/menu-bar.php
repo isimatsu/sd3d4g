cda3006a5b3f83b077f87172a67ef39f22b584af
kgryota <2301159@s.asojuku.ac.jp>
2025-10-08 12:40:42 +0900

    テンプレート作成

---
+++ assets/include/menu-bar.php
@@ -0,0 +1,24 @@
+        <a href="" class="menu-bar-btn">
+            <div>
+                <span class="material-symbols-rounded">home</span>
+                <p>ホーム</p>
+            </div>
+        </a>
+        <a href="" class="menu-bar-btn">
+            <div>
+                <span class="material-symbols-rounded">star_shine</span>
+                <p>提案</p>
+            </div>
+        </a>
+        <a href="" class="menu-bar-btn">
+            <div>
+                <span class="material-symbols-rounded">list</span>
+                <p>旅程一覧</p>
+            </div>
+        </a>
+        <a href="" class="menu-bar-btn">
+            <div>
+                <span class="material-symbols-rounded">music_note</span>
+                <p>音楽</p>
+            </div>
+        </a>
---
+++ pagename/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>pagename -旅行提案アプリ-</title>
+</head>
+<body>
+    <main>
+        <sction class="sm">
+            <div class="header">
+                <div>
+
+                </div>
+                <div class="header-account">
+                    <span class="material-symbols-rounded">account_circle</span>
+                    <p>アカウント名</p>
+                </div>
+            </div>
+            <div class="page-header">
+                <h1>pagetitle</h1>
+                <p>ここにページの説明が入ります。ここにページの説明が入ります</p>
+            </div>
+            <div class="page-contents">
+                <a href="" class="basic-btn blue-btn">はじめる</a>
+                <a href="" class="basic-btn gray-btn">キャンセル</a>
+            </div>
+        </sction>
+    </main>
+    <div class="menu-bar-area">
+        <?php include '../assets/include/menu-bar.php '?>
+    </div>
+</body>
+</html>

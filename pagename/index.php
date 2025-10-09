4b7f7ffb4ce19b8b1b055831171485007973f5df
kgryota <2301159@s.asojuku.ac.jp>
2025-10-09 10:58:19 +0900

    テンプレート完成

--- pagename/index.php
+++ pagename/index.php
@@ -6,6 +6,10 @@
     <link rel="stylesheet" type="text/css" href="../assets/css/reset.css">
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
     <title>pagename -旅行提案アプリ-</title>
 </head>
 <body>
@@ -15,10 +19,10 @@
                 <div>
 
                 </div>
-                <div class="header-account">
+                <a href="#" class="header-account">
                     <span class="material-symbols-rounded">account_circle</span>
                     <p>アカウント名</p>
-                </div>
+                </a>
             </div>
             <div class="page-header">
                 <h1>pagetitle</h1>
@@ -27,6 +31,53 @@
             <div class="page-contents">
                 <a href="" class="basic-btn blue-btn">はじめる</a>
                 <a href="" class="basic-btn gray-btn">キャンセル</a>
+
+                <div class="music-card">
+                    <div class="music-info">
+                        <p style="font-weight: bold; color: #E6B422 ;">#1</p>
+                        <img class="music-img" src="../assets/img/music_tmp_img.jpg">
+                        <p>曲名がはいる</p>
+                    </div>
+                    <div class="music-action-btn">
+                        <span class="music-play material-symbols-rounded">play_circle</span>
+                        <span class="music-favorite material-symbols-rounded">favorite</span>
+                    </div>
+                </div><!--music-card-->
+                <div class="music-card">
+                    <div class="music-info">
+                        <p style="font-weight: bold; color: #b5b5b4ff ;">#2</p>
+                        <img class="music-img" src="../assets/img/music_tmp_img.jpg">
+                        <p>曲名がはいる</p>
+                    </div>
+                    <div class="music-action-btn">
+                        <span class="music-play material-symbols-rounded">play_circle</span>
+                        <span class="music-favorite material-symbols-rounded">favorite</span>
+                    </div>
+                </div><!--music-card-->
+                <div class="music-card">
+                    <div class="music-info">
+                        <p style="font-weight: bold; color: #b87333 ;">#3</p>
+                        <img class="music-img" src="../assets/img/music_tmp_img.jpg">
+                        <p>曲名がはいる</p>
+                    </div>
+                    <div class="music-action-btn">
+                        <span class="music-play material-symbols-rounded">play_circle</span>
+                        <span class="music-favorite material-symbols-rounded">favorite</span>
+                    </div>
+                </div><!--music-card-->
+
+                <from action="#" class="basic-form">
+                    <div class="basic-form-box">
+                        <p class="input-name">ここに入力名</p>
+                        <input class="basic-form-input" type="text" placeholder="例：ここのプレースホルダです。">
+                    </div><!--basic-form-box-->
+                    <div class="basic-form-box">
+                        <p class="input-name">ここに入力名</p>
+                        <input class="basic-form-input" type="text" placeholder="例：ここのプレースホルダです。">
+                    </div><!--basic-form-box-->
+                </from><!--basic-form-->
+
+                
             </div>
         </sction>
     </main>

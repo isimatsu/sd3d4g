df602b3308c247b1276003b7fc2dd2aeafd86754
kgryota <2301159@s.asojuku.ac.jp>
2025-10-10 11:04:17 +0900

    テンプレート更新（アニメーション追加）

---
+++ assets/include/header.php
@@ -0,0 +1,7 @@
+                <div>
+
+                </div>
+                <a href="#" class="header-account">
+                    <span class="material-symbols-rounded">account_circle</span>
+                    <p>アカウント名</p>
+                </a>
---
+++ createplan/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <title>旅程作成 -旅行提案アプリ-</title>
+</head>
+<body>
+    <main>
+        <sction class="sm">
+            <div class="header">
+                <?php include '../assets/include/header.php '?>
+
+            </div>
+            <div class="page-header">
+                <h1>旅程の新規作成</h1>
+                <p>あなたに合った旅程を提案します。旅行先、日程、同行者を教えてください</p>
+            </div>
+            <div class="page-contents">
+
+            </div>
+        </sction>
+    </main>
+    <div class="menu-bar-area">
+        <?php include '../assets/include/menu-bar.php '?>
+    </div>
+</body>
+</html>

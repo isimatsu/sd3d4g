1ca429dee7d49d9a324ba0def4d89e8b2e14bef7
kentoAkama <2301122@s.asojuku.ac.jp>
2025-10-10 12:42:34 +0900

    login追加
    変更

---
+++ login/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <title>pagename -旅行提案アプリ-</title>
+</head>
+<body>
+    <main>
+        <sction class="sm">
+            <div class="header">
+                <div>
+
+                </div>
+                <a href="#" class="header-account">
+                    <span class="material-symbols-rounded">account_circle</span>
+                    <p>アカウント名</p>
+                </a>
+            </div>
+            <div class="page-header">
+                <h1>ログイン</h1>
+                <p>ログインします。アカウント情報を入力してください。
+                    またアカウントをお持ちでない方は<a href="../signup//index.php"新規登録
+            </div>
+            <div class="page-contents">
+
+            <from action="#" class="basic-form">
+                    <div class="basic-form-box">
+                        <p class="input-address">メールアドレス</p>
+                        <input class="basic-form-input" type="text" placeholder="aso@asojuku.ac.jp">
+                    </div><!--basic-form-box-->
+                    <div class="basic-form-box">
+                        <p class="input-password">パスワード</p>
+                        <input class="basic-form-input" type="text" >
+                    </div><!--basic-form-box-->
+                </from><!--basic-form-->
+
+                <div class="error-message">
+                    <p><strong>*:メールアドレスかパスワードが違います・</strong></p>
+                </div>
+
+
+                <a href="" class="basic-btn blue-btn">ログイン</a>
+                
+                
+
+                
+            </div>
+        </sction>
+    </main>
+    <div class="menu-bar-area">
+        <?php include '../assets/include/menu-bar.php'?>
+    </div>
+</body>
+
+</html>

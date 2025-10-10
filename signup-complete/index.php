bbac8bf7b54d4ee1f3b526fa95f383daab27007a
kgryota <2301159@s.asojuku.ac.jp>
2025-10-10 12:15:47 +0900

    登録完了

---
+++ signup-complete/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <title>アカウント作成完了 -旅行提案アプリ-</title>
+</head>
+<body>
+    <main>
+        <sction class="sm">
+            <div class="header">
+                <div>
+
+                </div>
+                <a href="#" class="header-account">
+                    <span class="material-symbols-rounded">account_circle</span>
+                    <p>アカウント名</p>
+                </a>
+            </div>
+            <div class="page-contents">
+                <div class="page-center-content">
+                    <div class="welcome">
+                        <img src="../assets/img/ariflight.png">
+                        <h1>ようこそ</h1>
+                        <p>アカウント登録が完了しました。これからあなたにぴったりの旅をご提案します。</p>
+                        <a href="" class="basic-btn blue-btn">はじめる</a>
+                    </div>
+                </div>
+            </div>
+        </sction>
+    </main>
+    <div class="menu-bar-area">
+        <?php include '../assets/include/menu-bar.php'?>
+    </div>
+</body>
+</html>

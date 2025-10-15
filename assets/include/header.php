90ff310283aceba1cf6ae9b4ca486e827470764a
isimatsu <2301128@s.asojuku.ac.jp>
2025-10-15 11:58:59 +0900

    ヘッダー

--- assets/include/header.php
+++ assets/include/header.php
@@ -1,12 +1,14 @@
 <?php
+session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-    session_start();
-    if(isset($_SESSION['user_id'])){
+if(isset($_SESSION['user_id'])){
         $user_id = $_SESSION['user_id'];
         $user_name = $_SESSION['user_name'];
     }
+
+    
 ?>
 <div>
 </div>

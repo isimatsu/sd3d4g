069e20a194fc6fd7fd3ea1a1f82a686849d068e9
kentoAkama <2301122@s.asojuku.ac.jp>
2025-10-23 10:11:57 +0900

    plan-delete完成
    完成

--- plan-delete/index.php
+++ plan-delete/index.php
@@ -43,61 +43,13 @@ error_reporting(E_ALL);
             <div class="header">
                 <?php include '../assets/include/header.php'?>
             </div>
-            <div class="page-header">
+             <div class="page-header">
                 <h1>旅程一覧</h1>
                 <p>過去に生成した旅程の一覧です</p>
             </div>
-            <div class="page-contents">
-                </a><!--plan-card-->
-                <!-- $tripsが空かどうか -->
-                <?php if(count($trips) > 0): ?>
-                    <?php foreach($trips as $trip): ?>
-                    <!-- リンクが決まり次第加筆してください -->
-                    <a href="../plan?plan_id=<?= htmlspecialchars($trip['trip_id'])?>"
-                        class="plan-card main-card" 
-                        style="background-image: url(../assets/img/spot_img/1.jpg);">
-
-                        <div class="plan-card-detail">
-                            <h2><?= htmlspecialchars($trip['trip_name']) ?></h2>
-                            <?php if (!empty($trip['trip_start']) && !empty($trip['trip_end'])): ?>
-                                <p>
-                                    <?= date('Y/m/d', strtotime($trip['trip_start'])) ?> ～ 
-                                    <?= date('Y/m/d', strtotime($trip['trip_end'])) ?>
-                                </p>
-                            <?php endif; ?>
-                        </div>
-                        <a href="../" class="delete-btn"><span class="material-symbols-rounded">delete</span></a>
-                    </a>
-                    <?php endforeach; ?>
-                <?php else: ?><!-- 旅程が空だった場合は以下を表示 -->
-                    <div class="empty-state">
-                        <span class="material-symbols-rounded" style="font-size: 60px;">event_busy</span>
-                        <p >旅程一覧がありません</p>
-                    </div>
-                <?php endif; ?>
-
-                        <!-- 元々のコードは一応残しておきます
-                        <div class="plan-card-detail">
-                            <div>
-                                <h2>北海道旅行</h2>
-                                <p>2025/10/10 ~ 2025/10/12</p>
-                            </div>
-                        </div>
-                        <div class="delete-btn"><span class="material-symbols-rounded">delete</span></div>
-                    </a>--plan-card--
-                    <a href="" class="plan-card side-card" style="background-image: url(../assets/img/spot_img/40.jpg);">
-                        <div class="plan-card-detail">
-                            <div>
-                                <h2>福岡旅行</h2>
-                                <p>2025/10/10 ~ 2025/10/12</p>
-                            </div>
-                        </div>
-                        <div class="delete-btn"><span class="material-symbols-rounded">delete</span></div>
-                    </a>--plan-card--
-                    -->
-
-                    
-
+            <div class="empty-state">
+                <span class="material-symbols-rounded" style="font-size: 60px;">delete</span>
+                <p >旅程一覧を削除しました</p>
             </div>
         </sction>
     </main>

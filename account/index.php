1f64d1947bec4a173ca5c9e76951d1aa1c399d96
isimatsu <2301128@s.asojuku.ac.jp>
2025-12-12 12:14:22 +0900

    エラー防止

--- account/index.php
+++ account/index.php
@@ -3,6 +3,10 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+if (!isset($_SESSION['user_id'])) {
+            header('Location: ../signin/index.php');
+            exit;
+        }
     
 
     if(isset($_SESSION['user_id'])){

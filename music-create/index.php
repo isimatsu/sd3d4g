1e8afe5b8ab5a7976d013a83a56ee3b5f0fe1f10
hitoshibito <2301189@s.asojuku.ac.jp>
2025-12-12 12:29:16 +0900

    楽曲登録画面、ログインチェック実装

--- music-create/index.php
+++ music-create/index.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+
+// ログインしていなければリダイレクト
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: ../signin/index.php');
+        exit;
+    }
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -8,35 +15,6 @@ $pdo=new PDO('mysql:host=mysql326.phy.lolipop.lan;
                 'LAA1682282',
                 'Passsd3d');
     
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
-
-    if ($email !== '' && $password !== '') {
-        $sql = $pdo->prepare('SELECT * FROM user WHERE email = ? AND password = ?');
-        $sql->execute([$email, $password]);
-
-        if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
-            $_SESSION['user_id'] = $row['user_id'];
-            $_SESSION['user_name'] = $row['user_name'];
-            header('Location: index.php');
-            exit;
-        } else {
-            echo 'メールアドレスまたはパスワードが違います。';
-            echo '<a href="./signin/index.php">戻る</a>';
-            exit;
-        }
-        } else {
-            echo 'メールアドレスとパスワードを入力してください。';
-            echo '<a href="./signin/index.php">戻る</a>';
-            exit;
-        }
-        } else {
-        if (!isset($_SESSION['user_id'])) {
-            header('Location: ../signin/index.php');
-            exit;
-        }
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -58,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="header">
                 <?php include '../assets/include/header.php'?>
             </div>
-            <button id="back-btn" type="button">戻る</button>
+            <button id="back-btn" type="button" style="color: #007bff;">戻る</button>
             <div class="page-header">
                 <h1>楽曲登録</h1>
                 <p style="text-align:center;">楽曲を登録します。必要な情報を入力してください。</p>
